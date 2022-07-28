a072df4b7906f6433f6058cf1747d3fec80fc810
Michael Phelps <mpphelps@gmail.com>
2022-07-28 13:18:15 -0400

    Fleshed out winning condition check

--- TicTacToe/src/main.cpp
+++ TicTacToe/src/main.cpp
@@ -29,7 +29,33 @@ public:
 
 	// To Do: Flesh out checking win status
 	bool IsInWinState() {
-		return (grid[0][0] == 'X')? true : false;
+		// Check Rows
+		for (int i = 0; i < sizeof(grid) / sizeof(grid[0]); i++)
+		{
+			if ((grid[i][0] == 'X') && (grid[i][1] == 'X') && (grid[i][2] == 'X'))
+				return true;
+			else if ((grid[i][0] == 'O') && (grid[i][1] == 'O') && (grid[i][2] == 'O'))
+				return true;
+		}
+		// Check Columns
+		for (int i = 0; i < sizeof(grid) / sizeof(grid[0]); i++)
+		{
+			if ((grid[0][i] == 'X') && (grid[1][i] == 'X') && (grid[2][i] == 'X'))
+				return true;
+			else if ((grid[0][i] == 'O') && (grid[1][i] == 'O') && (grid[2][i] == 'O'))
+				return true;
+		}
+		// Check Diagonals
+		if ((grid[0][0] == 'X') && (grid[1][1] == 'X') && (grid[2][2] == 'X'))
+			return true;
+		else if ((grid[0][0] == 'O') && (grid[1][1] == 'O') && (grid[2][2] == 'O'))
+			return true;
+		else if ((grid[2][0] == 'X') && (grid[1][1] == 'X') && (grid[0][2] == 'X'))
+			return true;
+		else if ((grid[2][0] == 'O') && (grid[1][1] == 'O') && (grid[0][2] == 'O'))
+			return true;
+		else 
+			return false;
 	}
 
 	void ResetBoard() {
@@ -93,7 +119,7 @@ public:
 			gameOver = true;
 			SwapPlayer();
 			std::cout << turn << " Wins!" << std::endl;
-			xScore++;
+			(turn == 'X') ? xScore++ : oScore++;
 		}
 		
 	}
